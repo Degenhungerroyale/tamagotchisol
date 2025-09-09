366e661c9b45c856ebb93a0f20421f5d010fa0ad
Degenhungerroyale <degenhungerroyale@gmail.com>
2025-09-09 11:55:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',   // ✅ ensures images load correctly on Netlify
+  base: '',   // ✅ use relative paths (works for previews + main site)
   build: {
     outDir: 'dist',
   },
