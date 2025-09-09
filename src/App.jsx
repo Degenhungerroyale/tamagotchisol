a82d0f401dc5f60d0f218a125922f7476ecc4bfe
Degenhungerroyale <degenhungerroyale@gmail.com>
2025-09-09 11:01:09 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TamagotchiGame from "./components/TamagotchiGame";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <TamagotchiGame />
+    </div>
+  );
+}
