b9fb0d6ce779d83f42a5d12a1fd5582377d781ea
Degenhungerroyale <degenhungerroyale@gmail.com>
2025-09-11 12:03:10 +0300

    Update TamagotchiGame.jsx

--- src/components/TamagotchiGame.jsx
+++ src/components/TamagotchiGame.jsx
@@ -3,14 +3,16 @@ import React from "react";
 const TamagotchiGame = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
-      {/* Tamagotchi frame container */}
-      <div className="relative w-[320px] h-[420px] bg-[url('/tamagotchi.png')] bg-contain bg-no-repeat">
-        
-        {/* Game screen (inside frame) */}
+      {/* Tamagotchi frame */}
+      <div
+        className="relative w-[320px] h-[420px] bg-contain bg-no-repeat"
+        style={{ backgroundImage: "url(/pets/tamagotchi.png)" }}
+      >
+        {/* Game screen */}
         <div className="absolute top-[25%] left-[20%] w-[60%] h-[40%] overflow-hidden flex flex-col items-center justify-center bg-black border-2 border-green-500 rounded">
           <h2 className="text-green-400 text-lg font-bold">Unnamed Dino</h2>
           <img
-            src="/dino.png"
+            src="/pets/dino.png"
             alt="Dino"
             className="w-16 h-16 my-2"
           />
@@ -18,7 +20,9 @@ const TamagotchiGame = () => {
           <div className="w-full h-2 bg-green-700 my-2">
             <div className="w-1/2 h-full bg-green-400"></div>
           </div>
-          <p className="text-red-500 text-xs">Rotate back to portrait to continue</p>
+          <p className="text-red-500 text-xs">
+            Rotate back to portrait to continue
+          </p>
           <button className="mt-2 px-3 py-1 bg-green-600 text-black rounded">
             Adopt (2 LOS)
           </button>
